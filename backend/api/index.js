9adbe1e7e616e37c81332c1dd4cce201d85e2691
whwp <teyhawap@gmail.com>
2023-03-18 19:20:14 -0700

    fifth commit

--- backend/api/index.js
+++ backend/api/index.js
@@ -21,6 +21,25 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // defining an endpoint to get the NFTs
+app.get("/", (req, res, next) => {
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  res.send({ detail: "ok" });
+});
+
+// defining an endpoint to get the NFTs
 app.get("/api", async (req, res, next) => {
   console.log("db point");
   console.log(req);
